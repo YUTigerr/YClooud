8f7dbdfa0e21db3843e16cb94f433f188e14ba06
Tiger4869 <y.yoshida@crl.epi.dendai.ac.jp>
2024-06-15 16:58:49 +0900

    :sparkles: Mapとオブジェクト

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const cooktailListInit = new Map<number, string>();
-cooktailListInit.set(2345, "ホワイトレディ");
-cooktailListInit.set(4412, "ブルーハワイ");
-cooktailListInit.set(6792, "ニューヨーク");
-const cooktailList = ref(cooktailListInit);
+const cooktailListInit = new Map<number, Cooktail>();
+cooktailListInit.set(2345, { id: 2345, name: "whiteLady", price: 1200 });
+cooktailListInit.set(4412, { id: 4412, name: "blueHawaii", price: 1500 });
+cooktailListInit.set(6792, { id: 6792, name: "newYork", price: 1100 });
+cooktailListInit.set(8429, { id: 8429, name: "martine", price: 1500 });
+const cooktailDataList = ref(cooktailListInit);
+
+interface Cooktail {
+  id: number;
+  name: string;
+  price?: number;
+}
 </script>
+
 <template>
   <ul>
-    <li v-for="[id, cooktaiName] in cooktailList" :key="id">
-      IDが{{ id }}のカクテルは{{ cooktaiName }}です
+    <li v-for="[id, cooktailItem] in cooktailDataList" :key="id">
+      {{ cooktailItem.name }}の値段は{{ cooktailItem.price }}円
     </li>
   </ul>
 </template>
